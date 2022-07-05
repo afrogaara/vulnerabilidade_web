a8ddb72fafc5fc5228c1e0f83cf8a3aef84095fc
Roberto César <robertocoliver@protonmail.com>
2022-07-05 16:56:58 -0300

    desserialização

--- Insecure Deserialization/teste.py
+++ Insecure Deserialization/teste.py
@@ -1,1 +1,15 @@
-.
+import pickle 
+
+class cla:
+    def __init__(self, uzumaki, uchiha):
+        self.uzumaki = uzumaki 
+        self.uchiha = uchiha 
+    def descricao(self):
+        return "Uzumaki: {}, Uchiha: {}".format(self.uzumaki, self.uchiha)
+   
+s = pickle.dumps(uzumaki1)
+print(f"serialized - >  {s}") 
+
+d = pickle.loads(s)
+print(f"deserialization - >  {d}")
+print(f"objeto - > {d.descricao()}")
