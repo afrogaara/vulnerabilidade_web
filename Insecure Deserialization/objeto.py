766eab8596a25fa35381c62f2499e04d2d8480bc
Roberto César <robertocoliver@protonmail.com>
2022-07-05 16:49:16 -0300

    Update and rename teste.py to Insecure Deserialization/objeto.py

---
+++ Insecure Deserialization/objeto.py
@@ -0,0 +1,13 @@
+import pickle 
+
+class cla:
+    def __init__(self, uzumaki, uchiha):
+        self.uzumaki = uzumaki 
+        self.uchiha = uchiha 
+    def descricao(self):
+        return "Uzumaki: {}, Uchiha: {}".format(self.uzumaki, self.uchiha)
+    
+uzumaki1 = cla("naruto", "sasuke")
+print(f"objeto - > {uzumaki1}")
+print(f"descrição do objeto - > {uzumaki1.descricao()}")
+
