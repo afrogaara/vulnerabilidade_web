18fc36a637f039ce8a0aba70e0bd3d52ef2aa866
Roberto César <robertocoliver@protonmail.com>
2022-07-06 01:10:21 -0300

    Update exemplo1.py

--- Insecure Deserialization/exemplo1.py
+++ Insecure Deserialization/exemplo1.py
@@ -6,6 +6,10 @@ a = ['joao', 'maria', 'pedro', 'jonas']
 serialized = base64.b64encode(pickle.dumps(a)) #- > transforma em base64(objeto) e dps faz a serialização 
 print(serialized)
 
+#desserialização
+import pickle 
+import base64 
 
-
-
+data = input(" ")
+deserialized = pickle.loads(base64.b64decode(data)) 
+print(deserialized)
